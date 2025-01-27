52bebce21478f912ab24da3ca3753a49881f6dda
Yuhao David Xia <yuhao.xia@enti.cat>
2025-01-27 21:51:25 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+"FILE_NAMEi/bin/bash
+
 
 PORT="2022"
 
@@ -7,40 +8,59 @@ echo "Servidor de Dragón Magia Abuelita Miedo 2022"
 echo "0. ESCUCHAMOS"
 DATA=`nc -l $PORT`
 
-HEADER=`echo "$DATA" | cut -d " " -f 1`
-IP=`echo "$DATA" | cut -d " " -f 2`
-
-if [ "$HEADER" != "DMAM" ]
+if [ "$DATA" != "DMAM" ]
 then
 	echo "ERROR 1: Cabecera incorrecta"
-	echo "KO_HEADER" | nc $IP $PORT
+	echo "KO_HEADER" | nc localhost $PORT
 	exit 1
 fi
 
-echo "La IP del cliente es: $IP"
-
 echo "2. CHECK OK - Enviando OK_HEADER"
-echo "OK_HEADER" | nc $IP $PORT
-
+echo "OK_HEADER" | nc localhost $PORT
 DATA=`nc -l $PORT`
 
-echo "5. COMPROVANDO PREFIJO"
-
+echo "5. COMPROBANDO PREFIJO"
 PREFIX=`echo "$DATA" | cut -d " " -f 1`
+NOMBRE_ARCHIVO=`echo "$DATA" | cut -d " " -f 2`
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
-	echo "ERROR 2: Prefijo incorrecto"
-	echo "KO_FILE_NAME" | nc $IP $PORT
+	echo "ERROR 2: Prefijo  incorrecto"
+	echo "KO_FILE_NAME" | nc localhost $PORT
 	exit 2
 fi
 
-echo "6. ENVIAR OK_FILE_NAME"
-echo "OK_FILE_NAME" | nc $IP $PORT
-
+echo "6. ENVIANDO OK_FILE_NAME"
+echo "OK_FILE_NAME" | nc localhost $PORT
 DATA=`nc -l $PORT`
 
-echo "9. RECIBIENDO/ALMACENANDO ARCHIVO"
-echo "$DATA" > Server/dragon.txt
+
+echo "8. RECIBIENDO Y ALMACENANDO DATOS"
+if [ "$DATA" == "" ]
+then
+	echo "ERROR 3: Datos incorrectos"
+	echo "KO_DATA" | nc localhost $PORT
+	exit 3
+fi
+
+echo "$DATA" > server/dragon.txt
+
+echo "9. CHECK Y RESPUESTA"
+echo "OK_DATA"| nc localhost $PORT
+DATA=`nc -l $PORT`
+
+echo "12. RECIBIENDO MD5"
+PREFIX=`echo "$DATA" | cut -d " " -f 1`
+NOMBRE_ARCHIVO=`echo "$DATA" | cut -d " " -f 2`
+
+echo "13. CHECK MD5 - ENVIANDO OK_FILE_MD5
+if [ "$PREFIX" != "FILE_MD5" ]
+then
+	echo "ERROR 4: Prefijo  incorrecto"
+	echo "KO_FILE_MD5" | nc localhost $PORT
+	exit 4
+fi
+
+echo "OK_FILE_MD5"| nc localhost $PORT
 
 
