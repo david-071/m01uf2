43eb03a30ecc8aca90bb055f561a1cfd4963a28a
Yuhao David Xia <yuhao.xia@enti.cat>
2025-01-27 21:50:58 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,49 +1,45 @@
-#!/bin/bash
-
-if [ "$1" == "" ]
-then
-	echo "Debes indicar la dirección del servidor."
-	echo "Ejemplo:"
-	echo -e "\t$0 127.0.0.1"
-	exit 1
-fi
-
-IP_SERVER=$1
-
-IP=`ip a | grep "scope global" | xargs | cut -d " " -f 2 | cut -d "/" -f 1`
+#1! /bin/bash
 
 PORT="2022"
 
 echo "Cliente de Dragón Magia Abuelita Miedo 2022"
 
-echo "1. ENVÍO DE CABECERA"
-echo "DMAM $IP" | nc $IP $PORT
-
+echo "1. ENVIO DE CABECERA"
+echo "DMAM" | nc localhost $PORT
 DATA=`nc -l $PORT`
 
-echo "3. COMPROVANDO OK_HEADER"
+echo "3. COMPROBANDO HEADER"
 if [ "$DATA" != "OK_HEADER" ]
 then
 	echo "ERROR 1: El header se envió incorrectamente"
 	exit 1
 fi
 
-echo "4. ENVIAR FILE_NAME"
+echo "4. CHECK OK - Enviando FILE_NAME"
 
 FILE_NAME="dragon.txt"
-
-echo "FILE_NAME $FILE_NAME" | nc $IP $PORT
-
+echo "FILE_NAME $FILE_NAME" | nc localhost $PORT
 DATA=`nc -l $PORT`
 
-echo "7. COMPROVANDO OK_FILE_NAME"
 if [ "$DATA" != "OK_FILE_NAME" ]
 then
-	echo "ERROR 2: El nombre se envió incorrectamente"
+	echo "ERROR 2: El prefijo se envió incorrectamente"
 	exit 2
 fi
 
-echo "8. ENVIAR ARCHIVO"
-cat Client/dragon.txt | nc $IP $PORT
-
+echo "7. CHECK OK - Enviando contenido del archivo"
+cat client/$FILE_NAME | nc localhost $PORT
 DATA=`nc -l $PORT`
+
+echo "10. COMPROBANDO RESPUESTA"
+if [ "$DATA" != "OK_DATA" ]
+then
+	echo "ERROR 3: Los datos se enviarón incorrectamente"
+ 	exit 3
+fi
+
+echo "11. ENVIANDO MD5"
+echo "FILE_MD5 MD5SUM" | nc localhost $PORT
+DATA=`nc -l $PORT`
+
+echo "14. FIN"
