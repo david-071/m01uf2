ed655544a0b456ad02fdf12ca44c4fe4382ca29d
Yuhao David Xia <yuhao.xia@enti.cat>
2025-01-28 23:48:30 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -19,15 +19,25 @@ echo "2. CHECK OK - Enviando OK_HEADER"
 echo "OK_HEADER" | nc localhost $PORT
 DATA=`nc -l $PORT`
 
-echo "5. COMPROBANDO PREFIJO"
+echo "5. COMPROBANDO PREFIJO y MD5SUM"
 PREFIX=`echo "$DATA" | cut -d " " -f 1`
 NOMBRE_ARCHIVO=`echo "$DATA" | cut -d " " -f 2`
+MD5SUM_CLIENT=`echo "$DATA" | cut -d " " -f 3`
 
 if [ "$PREFIX" != "FILE_NAME" ]
 then
 	echo "ERROR 2: Prefijo  incorrecto"
 	echo "KO_FILE_NAME" | nc localhost $PORT
 	exit 2
+fi
+
+MD5SUM_OBTENIDO=`echo -n "NOMBRE_ARCHIVO" | md5sum`
+
+if [ "$MD5SUM_CLIENTE" != "$MD5SUM_OBTENIDO" ]
+then
+	echo "ERROR 3: El MD5 del nombre del archivo es incorrecto"
+	echo "KO_FILE_NAME_MD5" | nc localhost $PORT
+	exit 3
 fi
 
 echo "6. ENVIANDO OK_FILE_NAME"
@@ -38,9 +48,9 @@ DATA=`nc -l $PORT`
 echo "8. RECIBIENDO Y ALMACENANDO DATOS"
 if [ "$DATA" == "" ]
 then
-	echo "ERROR 3: Datos incorrectos"
+	echo "ERROR 4: Datos incorrectos"
 	echo "KO_DATA" | nc localhost $PORT
-	exit 3
+	exit 4
 fi
 
 echo "$DATA" > server/dragon.txt
@@ -51,15 +61,24 @@ DATA=`nc -l $PORT`
 
 echo "12. RECIBIENDO MD5"
 PREFIX=`echo "$DATA" | cut -d " " -f 1`
-NOMBRE_ARCHIVO=`echo "$DATA" | cut -d " " -f 2`
+MD5SUM_RECIBIDO=`echo "$DATA" | cut -d " " -f 2`
+MD5SUM_OBTENIDO=`md5sum server/dragon.txt | cut -d " " -f 1`
 
-echo "13. CHECK MD5 - ENVIANDO OK_FILE_MD5
+echo "13. CHECK Prefix y MD5 - ENVIANDO OK_FILE_MD5"
 if [ "$PREFIX" != "FILE_MD5" ]
 then
-	echo "ERROR 4: Prefijo  incorrecto"
+	echo "ERROR 5: Prefijo  incorrecto"
 	echo "KO_FILE_MD5" | nc localhost $PORT
-	exit 4
+	exit 5
 fi
+
+if [ "$MD5SUM_RECIBIDO" != "$MD5SUM_OBTENIDO" ]
+then
+	echo "ERROR 6: El MD5 del contenido del archivo es incorrecto"
+	echo "KO_FILE_MD5" | nc localhost $PORT
+	exit 6
+fi
+
 
 echo "OK_FILE_MD5"| nc localhost $PORT
 
